0d25ec2e622e20e42d5531da3972d5ac6825f608
Vineogs <vineogs.2001@gmail.com>
2023-09-05 13:37:41 -0300

    adição página adicionar

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,21 @@
+# Use a imagem oficial do PHP 8
 FROM php:8.0-apache
 
+# Crie um diretório dentro da imagem para o seu aplicativo
 WORKDIR /var/www/html
 
-COPY . /var/www/html/
+# Copie os arquivos PHP para o diretório de trabalho
+COPY php/ .
+
+# Copie os arquivos JavaScript e CSS para seus respectivos diretórios
+COPY resources/js/ /var/www/html/resources/js/
+COPY resources/style/ /var/www/html/resources/style/
+
+# Exponha a porta 80 para acesso do navegador
+EXPOSE 80
+
+# Comando para iniciar o servidor web PHP embutido
+CMD ["php", "-S", "0.0.0.0:80"]
 
 RUN a2enmod rewrite
 
---
+++ html/adicionar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>background-color: black;
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../resources/style/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <title>Game List - Adicionar</title>
+</head>
+<body id="bg">
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">LOGO</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lançamentos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Listas
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Zerados</a></li>
+                            <li><a class="dropdown-item" href="#">Platinados</a></li>
+                            <li><a class="dropdown-item" href="#">Para Jogar</a></li>
+                            <li><a class="dropdown-item" href="#">Dropados</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="adicionar.php">Adicionar</a>
+                    </li>
+                </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
